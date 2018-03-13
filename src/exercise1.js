7489a66aa2375b5d63876fd4b9b886f26f6b5bc9
savelier <valentinsaveliersen@gmail.com>
2018-03-13 10:31:30 +0100

    exercice1

--- src/exercise1.js
+++ src/exercise1.js
@@ -2,6 +2,36 @@ var removeNegativeValue = function(values) {
   // Your future job begins here ...
 }
 
+var removeNegativeValue = function(input)
+{
+  if (input === undefined)
+  {
+    var output = [];
+    return output;
+  }
+  else
+  {
+    var output = input.filter(function(number)
+      {
+        return number >= 0;
+      })
+    return output;
+  }
+}
+
+module.exports = {
+  title: 'Exercise 1',
+  run: removeNegativeValue
+}
+
+
+
+
+
+
+
+
+	
 module.exports = {
   title: 'Exercise 1',
   run: removeNegativeValue
