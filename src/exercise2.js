88ca5370b2d91d5afb17b69867550d7b38f76ea6
savelier <valentinsaveliersen@gmail.com>
2018-03-13 10:48:30 +0100

    evaluation

--- src/exercise2.js
+++ src/exercise2.js
@@ -2,6 +2,29 @@ var mapCustomer = function(customer) {
   // Your future job begins here ...
 }
 
+ var mapCustomer = function(input)
+ {
+ 	if var input = {
+      firstname: 'Jack',
+      lastname: 'Smith',
+      products: 
+      [{ label: 'shoes', price: 89 }, 
+      { label: 'book', price: 14 },
+       { label: 'smartphone', price: 499 }]
+       return input 
+    }
+ 		
+    var output = {
+    	firstname: 'John',
+      lastname: 'Doe',
+      products: [],
+      moneySpent: 0;
+    return output;
+  }
+  else
+ }
+
+
 module.exports = {
   title: 'Exercise 2',
   run: mapCustomer
